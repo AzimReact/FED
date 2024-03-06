e03925f477afe7787d031877b0ee7c62bc34e3b5
Azim <azimreact@gmail.com>
2024-03-06 21:17:41 +0600

    custom mobileLoginBtn

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,19 +1,21 @@
-// -----mobile------
+// // -----mobile------
 
-const dropdownBtn = document.querySelector(".dropdown-icon")
-const dropdownContent = document.querySelector(".dropdown-content")
-const dropdownCloseBtn = document.querySelector(".dropdown-header-closeBtn")
+// const dropdownBtn = document.querySelector(".dropdown-icon")
+// const dropdownContent = document.querySelector(".dropdown-content")
+// const dropdownCloseBtn = document.querySelector(".dropdown-header-closeBtn")
+// const dropdownLoginBtn = document.getElementById("dropdown-loginBtn")
 
-function openDropdown() {
-  const overlay = document.getElementById("overlay")
-  dropdownContent.style.display = 'block'
-  overlay.style.display = "block"
-}
-function closeDropdown() {
-  const overlay = document.getElementById("overlay")
-  dropdownContent.style.display = 'none'
-  overlay.style.display = "none"
-}
+// function openDropdown() {
+//   const overlay = document.getElementById("overlay")
+//   dropdownContent.style.display = 'block'
+//   overlay.style.display = "block"
+// }
+// function closeDropdown() {
+//   const overlay = document.getElementById("overlay")
+//   dropdownContent.style.display = 'none'
+//   overlay.style.display = "none"
+// }
 
-dropdownBtn.onclick = openDropdown
-dropdownCloseBtn.onclick = closeDropdown
+// dropdownBtn.onclick = openDropdown
+// dropdownCloseBtn.onclick = closeDropdown
+// dropdownLoginBtn.onclick = closeDropdown
--- script/modal.js
+++ script/modal.js
@@ -18,3 +18,30 @@ function closeModal() {
   overlay.style.display = "none"
   modal.style.display = "none"
 }
+
+// -----логика дропдауна mobile------
+
+const dropdownBtn = document.querySelector(".dropdown-icon")
+const dropdownContent = document.querySelector(".dropdown-content")
+const dropdownCloseBtn = document.querySelector(".dropdown-header-closeBtn")
+const dropdownLoginBtn = document.getElementById("dropdown-loginBtn")
+
+function openDropdown() {
+  const overlay = document.getElementById("overlay")
+  dropdownContent.style.display = 'block'
+  overlay.style.display = "block"
+}
+function closeDropdown() {
+  const overlay = document.getElementById("overlay")
+  dropdownContent.style.display = 'none'
+  overlay.style.display = "none"
+}
+
+function loginBtnClick() {
+  closeDropdown()
+  openModal()
+}
+
+dropdownBtn.onclick = openDropdown
+dropdownCloseBtn.onclick = closeDropdown
+dropdownLoginBtn.onclick = loginBtnClick
