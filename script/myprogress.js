49dfe768c6fbc970dccdc396821b343a79684b7b
Azim <azimreact@gmail.com>
2024-03-06 21:01:08 +0600

    fix progress page

--- script/myprogress.js
+++ script/myprogress.js
@@ -5,7 +5,13 @@ let savedCounterValue = localStorage.getItem("savedCounterValue")
 
 if (savedCounterValue !== null) {
   counterValue = parseInt(savedCounterValue)
-  counterElement.textContent = counterValue
+  let hours = Math.floor(counterValue / 3600)
+  let minutes = Math.floor((counterValue % 3600) / 60)
+  let seconds = counterValue % 60
+
+  let formattedTime = hours + 'часов' + ' ' + minutes + 'минут' + ' ' + seconds + 'секунд!'
+
+  counterElement.textContent = formattedTime
 } else {
   counterElement.textContent = 0
 }
